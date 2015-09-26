28268a9d2dcc1d9c7f64fdb0e5a547070741095a
Brian Muramatsu <btmura@gmail.com>
2015-09-25 22:35:11 -0700

    add glfw sample usage code

--- blockcillin.go
+++ blockcillin.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"runtime"
+
+	"github.com/go-gl/glfw/v3.1/glfw"
+)
+
+func init() {
+	// This is needed to arrange that main() runs on the main thread.
+	// See documentation for functions that are only allowed to be called from the main thread.
+	runtime.LockOSThread()
+}
 
 func main() {
-	fmt.Println("hello world")
+	if err := glfw.Init(); err != nil {
+		log.Fatalf("glfw.Init: %v", err)
+	}
+	defer glfw.Terminate()
+
+	win, err := glfw.CreateWindow(640, 480, "Testing", nil, nil)
+	if err != nil {
+		log.Fatalf("glfw.CreateWindow: %v", err)
+	}
+
+	win.MakeContextCurrent()
+
+	for !win.ShouldClose() {
+		// Do OpenGL stuff.
+		win.SwapBuffers()
+		glfw.PollEvents()
+	}
 }
