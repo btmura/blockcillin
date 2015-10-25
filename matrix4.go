a02efc65893d41aac374fce8d72b008987fdac41
Brian Muramatsu <btmura@gmail.com>
2015-10-24 21:28:28 -0700

    add view matrix function

--- matrix4.go
+++ matrix4.go
@@ -5,10 +5,10 @@ import "math"
 // Matrix4 is a 4x4 matrix.
 type Matrix4 [16]float32
 
-func makePerspective(fovRadians, aspect, near, far float64) Matrix4 {
+func makePerspectiveMatrix(fovRadians, aspect, near, far float64) *Matrix4 {
 	f := math.Tan(math.Pi*0.5 - 0.5*fovRadians)
 	rangeInv := 1.0 / (near - far)
-	return Matrix4{
+	return &Matrix4{
 		float32(f / aspect), 0, 0, 0,
 		0, float32(f), 0, 0,
 		0, 0, float32((near + far) * rangeInv), -1,
@@ -16,8 +16,25 @@ func makePerspective(fovRadians, aspect, near, far float64) Matrix4 {
 	}
 }
 
-func makeTranslationMatrix(x, y, z float32) Matrix4 {
-	return Matrix4{
+func makeViewMatrix(cameraPosition, target, up *Vector3) *Matrix4 {
+	cameraMatrix := makeLookAtMatrix(cameraPosition, target, up)
+	return cameraMatrix.Inverse()
+}
+
+func makeLookAtMatrix(cameraPosition, target, up *Vector3) *Matrix4 {
+	zAxis := cameraPosition.Sub(target).Normalize()
+	xAxis := up.Cross(zAxis)
+	yAxis := zAxis.Cross(xAxis)
+	return &Matrix4{
+		xAxis.x, xAxis.y, xAxis.z, 0,
+		yAxis.x, yAxis.y, yAxis.z, 0,
+		zAxis.x, zAxis.y, zAxis.z, 0,
+		cameraPosition.x, cameraPosition.y, cameraPosition.z, 1,
+	}
+}
+
+func makeTranslationMatrix(x, y, z float32) *Matrix4 {
+	return &Matrix4{
 		1, 0, 0, 0,
 		0, 1, 0, 0,
 		0, 0, 1, 0,
@@ -25,10 +42,10 @@ func makeTranslationMatrix(x, y, z float32) Matrix4 {
 	}
 }
 
-func makeXRotationMatrix(radians float64) Matrix4 {
+func makeXRotationMatrix(radians float64) *Matrix4 {
 	c := float32(math.Cos(radians))
 	s := float32(math.Sin(radians))
-	return Matrix4{
+	return &Matrix4{
 		1, 0, 0, 0,
 		0, c, s, 0,
 		0, -s, c, 0,
@@ -36,10 +53,10 @@ func makeXRotationMatrix(radians float64) Matrix4 {
 	}
 }
 
-func makeYRotationMatrix(radians float64) Matrix4 {
+func makeYRotationMatrix(radians float64) *Matrix4 {
 	c := float32(math.Cos(radians))
 	s := float32(math.Sin(radians))
-	return Matrix4{
+	return &Matrix4{
 		c, 0, -s, 0,
 		0, 1, 0, 0,
 		s, 0, c, 0,
@@ -47,10 +64,10 @@ func makeYRotationMatrix(radians float64) Matrix4 {
 	}
 }
 
-func makeZRotationMatrix(radians float64) Matrix4 {
+func makeZRotationMatrix(radians float64) *Matrix4 {
 	c := float32(math.Cos(radians))
 	s := float32(math.Sin(radians))
-	return Matrix4{
+	return &Matrix4{
 		c, s, 0, 0,
 		-s, c, 0, 0,
 		0, 0, 1, 0,
@@ -58,8 +75,8 @@ func makeZRotationMatrix(radians float64) Matrix4 {
 	}
 }
 
-func makeScaleMatrix(sx, sy, sz float32) Matrix4 {
-	return Matrix4{
+func makeScaleMatrix(sx, sy, sz float32) *Matrix4 {
+	return &Matrix4{
 		sx, 0, 0, 0,
 		0, sy, 0, 0,
 		0, 0, sz, 0,
@@ -67,8 +84,8 @@ func makeScaleMatrix(sx, sy, sz float32) Matrix4 {
 	}
 }
 
-func multipleMatrices(m, n Matrix4) Matrix4 {
-	return Matrix4{
+func multipleMatrices(m, n *Matrix4) *Matrix4 {
+	return &Matrix4{
 		m[0]*n[0] + m[1]*n[4] + m[2]*n[8] + m[3]*n[12],
 		m[0]*n[1] + m[1]*n[5] + m[2]*n[9] + m[3]*n[13],
 		m[0]*n[2] + m[1]*n[6] + m[2]*n[10] + m[3]*n[14],
@@ -88,5 +105,74 @@ func multipleMatrices(m, n Matrix4) Matrix4 {
 		m[12]*n[1] + m[13]*n[5] + m[14]*n[9] + m[15]*n[13],
 		m[12]*n[2] + m[13]*n[6] + m[14]*n[10] + m[15]*n[14],
 		m[12]*n[3] + m[13]*n[7] + m[14]*n[11] + m[15]*n[15],
+	}
+}
+
+func (m *Matrix4) Inverse() *Matrix4 {
+	m00 := m[0*4+0]
+	m01 := m[0*4+1]
+	m02 := m[0*4+2]
+	m03 := m[0*4+3]
+	m10 := m[1*4+0]
+	m11 := m[1*4+1]
+	m12 := m[1*4+2]
+	m13 := m[1*4+3]
+	m20 := m[2*4+0]
+	m21 := m[2*4+1]
+	m22 := m[2*4+2]
+	m23 := m[2*4+3]
+	m30 := m[3*4+0]
+	m31 := m[3*4+1]
+	m32 := m[3*4+2]
+	m33 := m[3*4+3]
+	tmp_0 := m22 * m33
+	tmp_1 := m32 * m23
+	tmp_2 := m12 * m33
+	tmp_3 := m32 * m13
+	tmp_4 := m12 * m23
+	tmp_5 := m22 * m13
+	tmp_6 := m02 * m33
+	tmp_7 := m32 * m03
+	tmp_8 := m02 * m23
+	tmp_9 := m22 * m03
+	tmp_10 := m02 * m13
+	tmp_11 := m12 * m03
+	tmp_12 := m20 * m31
+	tmp_13 := m30 * m21
+	tmp_14 := m10 * m31
+	tmp_15 := m30 * m11
+	tmp_16 := m10 * m21
+	tmp_17 := m20 * m11
+	tmp_18 := m00 * m31
+	tmp_19 := m30 * m01
+	tmp_20 := m00 * m21
+	tmp_21 := m20 * m01
+	tmp_22 := m00 * m11
+	tmp_23 := m10 * m01
+
+	t0 := (tmp_0*m11 + tmp_3*m21 + tmp_4*m31) - (tmp_1*m11 + tmp_2*m21 + tmp_5*m31)
+	t1 := (tmp_1*m01 + tmp_6*m21 + tmp_9*m31) - (tmp_0*m01 + tmp_7*m21 + tmp_8*m31)
+	t2 := (tmp_2*m01 + tmp_7*m11 + tmp_10*m31) - (tmp_3*m01 + tmp_6*m11 + tmp_11*m31)
+	t3 := (tmp_5*m01 + tmp_8*m11 + tmp_11*m21) - (tmp_4*m01 + tmp_9*m11 + tmp_10*m21)
+
+	d := 1.0 / (m00*t0 + m10*t1 + m20*t2 + m30*t3)
+
+	return &Matrix4{
+		d * t0,
+		d * t1,
+		d * t2,
+		d * t3,
+		d * ((tmp_1*m10 + tmp_2*m20 + tmp_5*m30) - (tmp_0*m10 + tmp_3*m20 + tmp_4*m30)),
+		d * ((tmp_0*m00 + tmp_7*m20 + tmp_8*m30) - (tmp_1*m00 + tmp_6*m20 + tmp_9*m30)),
+		d * ((tmp_3*m00 + tmp_6*m10 + tmp_11*m30) - (tmp_2*m00 + tmp_7*m10 + tmp_10*m30)),
+		d * ((tmp_4*m00 + tmp_9*m10 + tmp_10*m20) - (tmp_5*m00 + tmp_8*m10 + tmp_11*m20)),
+		d * ((tmp_12*m13 + tmp_15*m23 + tmp_16*m33) - (tmp_13*m13 + tmp_14*m23 + tmp_17*m33)),
+		d * ((tmp_13*m03 + tmp_18*m23 + tmp_21*m33) - (tmp_12*m03 + tmp_19*m23 + tmp_20*m33)),
+		d * ((tmp_14*m03 + tmp_19*m13 + tmp_22*m33) - (tmp_15*m03 + tmp_18*m13 + tmp_23*m33)),
+		d * ((tmp_17*m03 + tmp_20*m13 + tmp_23*m23) - (tmp_16*m03 + tmp_21*m13 + tmp_22*m23)),
+		d * ((tmp_14*m22 + tmp_17*m32 + tmp_13*m12) - (tmp_16*m32 + tmp_12*m12 + tmp_15*m22)),
+		d * ((tmp_20*m32 + tmp_12*m02 + tmp_19*m22) - (tmp_18*m22 + tmp_21*m32 + tmp_13*m02)),
+		d * ((tmp_18*m12 + tmp_23*m32 + tmp_15*m02) - (tmp_22*m32 + tmp_14*m02 + tmp_19*m12)),
+		d * ((tmp_22*m22 + tmp_16*m02 + tmp_21*m12) - (tmp_20*m12 + tmp_23*m22 + tmp_17*m02)),
 	}
 }
---
+++ vector3.go
@@ -0,0 +1,33 @@
+package main
+
+import "math"
+
+type Vector3 struct {
+	x float32
+	y float32
+	z float32
+}
+
+func (v *Vector3) Sub(o *Vector3) *Vector3 {
+	return &Vector3{v.x - o.x, v.y - o.y, v.z - o.z}
+}
+
+func (v *Vector3) Length() float32 {
+	return float32(math.Sqrt(float64(v.x*v.x + v.y*v.y + v.z*v.z)))
+}
+
+func (v *Vector3) Cross(o *Vector3) *Vector3 {
+	return &Vector3{
+		v.y*o.z - v.z*o.y,
+		v.z*o.x - v.x*o.z,
+		v.x*o.y - v.y*o.x,
+	}
+}
+
+func (v *Vector3) Normalize() *Vector3 {
+	l := v.Length()
+	if l > 0.00001 {
+		return &Vector3{v.x / l, v.y / l, v.z / l}
+	}
+	return &Vector3{}
+}
