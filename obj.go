9f31e82e806e6b0abfe06c2f99d4c13e6a534eab
Brian Muramatsu <btmura@gmail.com>
2015-11-01 10:26:44 -0800

    add readObjVertex and readObjFace helper functions

--- obj.go
+++ obj.go
@@ -28,32 +28,47 @@ func ReadObjFile(r io.Reader) ([]*Obj, error) {
 		line := strings.TrimSpace(sc.Text())
 		switch {
 		case strings.HasPrefix(line, "v"):
-			v := &ObjVertex{}
-			if _, err := fmt.Sscanf(line, "v %f %f %f", &v.X, &v.Y, &v.Z); err != nil {
+			v, err := readObjVertex(line)
+			if err != nil {
 				return nil, err
 			}
 			obj.Vertices = append(obj.Vertices, v)
 
 		case strings.HasPrefix(line, "f"):
-			tokens := strings.Split(line, " ")
-			if len(tokens) < 4 || tokens[0] != "f" {
-				return nil, fmt.Errorf("invalid face spec: %q", line)
+			f, err := readObjFace(line)
+			if err != nil {
+				return nil, err
 			}
-
-			f := ObjFace{}
-			for i, t := range tokens {
-				if i == 0 {
-					continue
-				}
-				vi, err := strconv.Atoi(t)
-				if err != nil {
-					return nil, err
-				}
-				f = append(f, vi)
-			}
-
-			obj.Faces = append(obj.Faces, &f)
+			obj.Faces = append(obj.Faces, f)
 		}
 	}
 	return []*Obj{obj}, nil
+}
+
+func readObjVertex(line string) (*ObjVertex, error) {
+	v := &ObjVertex{}
+	if _, err := fmt.Sscanf(line, "v %f %f %f", &v.X, &v.Y, &v.Z); err != nil {
+		return nil, err
+	}
+	return v, nil
+}
+
+func readObjFace(line string) (*ObjFace, error) {
+	tokens := strings.Split(line, " ")
+	if len(tokens) < 4 || tokens[0] != "f" {
+		return nil, fmt.Errorf("invalid face spec: %q", line)
+	}
+
+	f := ObjFace{}
+	for i, t := range tokens {
+		if i == 0 {
+			continue
+		}
+		vi, err := strconv.Atoi(t)
+		if err != nil {
+			return nil, err
+		}
+		f = append(f, vi)
+	}
+	return &f, nil
 }
