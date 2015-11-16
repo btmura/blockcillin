1d8e0ce9aa11fa2716836094345e9881ed4e9d53
Brian Muramatsu <btmura@gmail.com>
2015-11-15 16:52:19 -0800

    reposition selector and blocks

--- blockcillin.go
+++ blockcillin.go
@@ -28,7 +28,7 @@ var (
 	directionalLight  = [3]float32{0.5, 0.5, 0.5}
 	directionalVector = [3]float32{0.5, 0.5, 0.5}
 
-	cameraPosition = vector3{0, 1, 3}
+	cameraPosition = vector3{0, 4, 12}
 
 	blockColorByObjID = map[string]blockColor{
 		"red":    red,
@@ -148,36 +148,6 @@ func main() {
 	sizeCallback(win, w, h)
 	win.SetSizeCallback(sizeCallback)
 
-	globalRotation := []float32{0, 0, 0}
-	win.SetKeyCallback(func(w *glfw.Window, key glfw.Key, scancode int, action glfw.Action, mods glfw.ModifierKey) {
-		if action != glfw.Press && action != glfw.Repeat {
-			return
-		}
-
-		switch key {
-		case glfw.KeyUp:
-			globalRotation[0] += 5
-
-		case glfw.KeyDown:
-			globalRotation[0] -= 5
-
-		case glfw.KeyLeft:
-			globalRotation[1] -= 5
-
-		case glfw.KeyRight:
-			globalRotation[1] += 5
-
-		case glfw.Key1:
-			globalRotation[2] -= 5
-
-		case glfw.Key0:
-			globalRotation[2] += 5
-
-		case glfw.KeyEscape:
-			win.SetShouldClose(true)
-		}
-	})
-
 	gl.Uniform1i(textureUniform, 0)
 	gl.ActiveTexture(gl.TEXTURE0)
 	gl.BindTexture(gl.TEXTURE_2D, texture)
@@ -190,29 +160,52 @@ func main() {
 
 	b := newBoard()
 
-	updateMatrix := func(row, col int) {
-		localRotationY := float32(360.0 / b.cellCount * col)
-		xq := newAxisAngleQuaternion(xAxis, toRadians(globalRotation[0]))
-		yq := newAxisAngleQuaternion(yAxis, toRadians(globalRotation[1]+localRotationY))
-		zq := newAxisAngleQuaternion(zAxis, toRadians(globalRotation[2]))
-		qm := newQuaternionMatrix(yq.mult(xq).mult(zq).normalize())
+	cellRotation := float32(360.0 / b.cellCount)
+	startRotation := cellRotation / 2
+	globalRotation := []float32{0, 0, 0}
 
-		m := newScaleMatrix(0.25, 0.25, 0.25)
-		m = m.mult(newTranslationMatrix(0, -0.5*float32(row), -1))
+	updateSelectorMatrix := func() {
+		m := newTranslationMatrix(0, 0, 4)
+		gl.UniformMatrix4fv(matrixUniform, 1, false, &m[0])
+	}
+
+	updateCellMatrix := func(row, col int) {
+		localRotationY := startRotation + cellRotation*float32(col)
+		yq := newAxisAngleQuaternion(yAxis, toRadians(globalRotation[1]+localRotationY))
+		qm := newQuaternionMatrix(yq.normalize())
+
+		m := newTranslationMatrix(0, -2.0*float32(row), 4)
 		m = m.mult(qm)
 		gl.UniformMatrix4fv(matrixUniform, 1, false, &m[0])
 	}
+
+	win.SetKeyCallback(func(w *glfw.Window, key glfw.Key, scancode int, action glfw.Action, mods glfw.ModifierKey) {
+		if action != glfw.Press && action != glfw.Repeat {
+			return
+		}
+
+		switch key {
+		case glfw.KeyLeft:
+			globalRotation[1] -= cellRotation
+
+		case glfw.KeyRight:
+			globalRotation[1] += cellRotation
+
+		case glfw.KeyEscape:
+			win.SetShouldClose(true)
+		}
+	})
 
 	gl.ClearColor(0, 0, 0, 0)
 	for !win.ShouldClose() {
 		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
-		updateMatrix(0, 0)
+		updateSelectorMatrix()
 		selectorMesh.drawElements()
 
 		for row, r := range b.rings {
 			for col, c := range r.cells {
-				updateMatrix(row, col)
+				updateCellMatrix(row, col)
 				meshByBlockColor[c.blockColor].drawElements()
 			}
 		}
