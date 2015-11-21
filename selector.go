db75df6e8a439d55ae0311f48572be30fa10ef94
Brian Muramatsu <btmura@gmail.com>
2015-11-21 11:44:39 -0800

    animate moving the selector

--- selector.go
+++ selector.go
@@ -2,8 +2,25 @@ package main
 
 import "math"
 
+type selectorState int32
+
+const (
+	static selectorState = iota
+	movingUp
+	movingDown
+)
+
+// numMoveFrames is the number of frames a move animation takes.
+const numMoveFrames = 10
+
 type selector struct {
+	// state is the state of the selector.
+	state selectorState
+
 	y float32
+
+	// moveFrame is the current frame in the move animation from 0 to numMoveFrames.
+	moveFrame int
 
 	// scale is the scale of the selector to make it pulse.
 	scale float32
@@ -13,14 +30,31 @@ type selector struct {
 }
 
 func (s *selector) moveUp() {
-	s.y--
+	s.state = movingUp
 }
 
 func (s *selector) moveDown() {
-	s.y++
+	s.state = movingDown
 }
 
 func (s *selector) update() {
+	updateState := func() {
+		if s.moveFrame++; s.moveFrame == numMoveFrames {
+			s.state = static
+			s.moveFrame = 0
+		}
+	}
+
+	switch s.state {
+	case movingUp:
+		s.y -= 0.1
+		updateState()
+
+	case movingDown:
+		s.y += 0.1
+		updateState()
+	}
+
 	s.scale = float32(1.0 + math.Sin(float64(s.pulse)*0.1)*0.025)
 	s.pulse++
 }
