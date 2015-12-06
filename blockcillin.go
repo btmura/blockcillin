8ede788163bff7633e9c49de372b684b2402aae4
Brian Muramatsu <btmura@gmail.com>
2015-12-05 18:14:28 -0800

    move more rendering code into the renderer

--- blockcillin.go
+++ blockcillin.go
@@ -20,11 +20,19 @@ const (
 )
 
 var (
-	matrixUniform  int32
-	textureUniform int32
+	matrixUniform     int32
+	textureUniform    int32
+	grayscaleUniform  int32
+	brightnessUniform int32
+	alphaUniform      int32
 )
 
-var menuMesh *mesh
+var (
+	menuMesh       *mesh
+	selectorMesh   *mesh
+	blockMeshes    = map[blockColor]*mesh{}
+	fragmentMeshes = map[blockColor][4]*mesh{}
+)
 
 var (
 	xAxis = vector3{1, 0, 0}
@@ -40,6 +48,13 @@ var (
 	directionalVector = [3]float32{0.5, 0.5, 0.5}
 
 	cameraPosition = vector3{0, 5, 25}
+)
+
+const (
+	nw = iota
+	ne
+	se
+	sw
 )
 
 func init() {
@@ -100,16 +115,7 @@ func main() {
 	}
 
 	menuMesh = mm("menu")
-	selectorMesh := mm("selector")
-	blockMeshes := map[blockColor]*mesh{}
-	fragmentMeshes := map[blockColor][4]*mesh{}
-
-	const (
-		nw = iota
-		ne
-		se
-		sw
-	)
+	selectorMesh = mm("selector")
 
 	for c, id := range colorObjIDs {
 		blockMeshes[c] = mm(id)
@@ -149,13 +155,13 @@ func main() {
 	textureUniform, err = getUniformLocation(program, "u_texture")
 	logFatalIfErr("getUniformLocation", err)
 
-	grayscaleUniform, err := getUniformLocation(program, "u_grayscale")
+	grayscaleUniform, err = getUniformLocation(program, "u_grayscale")
 	logFatalIfErr("getUniformLocation", err)
 
-	brightnessUniform, err := getUniformLocation(program, "u_brightness")
+	brightnessUniform, err = getUniformLocation(program, "u_brightness")
 	logFatalIfErr("getUniformLocation", err)
 
-	alphaUniform, err := getUniformLocation(program, "u_alpha")
+	alphaUniform, err = getUniformLocation(program, "u_alpha")
 	logFatalIfErr("getUniformLocation", err)
 
 	gl.Uniform3fv(ambientLightUniform, 1, &ambientLight[0])
@@ -222,178 +228,6 @@ func main() {
 		}
 	})
 
-	cellRotationY := float32(360.0 / b.cellCount)
-	startRotationY := cellRotationY / 2
-	cellTranslationY := float32(2.0)
-
-	globalTranslationY := float32(0)
-	globalTranslationZ := float32(4)
-
-	selectorRelativeX := func(fudge float32) float32 {
-		move := func(delta float32) float32 {
-			return linear(s.step+fudge, float32(s.x), delta, numMoveSteps)
-		}
-
-		switch s.state {
-		case selectorMovingLeft:
-			return move(-1)
-
-		case selectorMovingRight:
-			return move(1)
-		}
-
-		return float32(s.x)
-	}
-
-	selectorRelativeY := func(fudge float32) float32 {
-		move := func(delta float32) float32 {
-			return linear(s.step+fudge, float32(s.y), delta, numMoveSteps)
-		}
-
-		switch s.state {
-		case selectorMovingUp:
-			return move(-1)
-		case selectorMovingDown:
-			return move(1)
-		}
-		return float32(s.y)
-	}
-
-	boardRelativeY := func(fudge float32) float32 {
-		return linear(b.riseStep+fudge, float32(b.y), 1, numRiseSteps)
-	}
-
-	blockRelativeX := func(b *block, fudge float32) float32 {
-		move := func(start, delta float32) float32 {
-			return linear(b.step+fudge, start, delta, numSwapSteps)
-		}
-
-		switch b.state {
-		case blockSwappingFromLeft:
-			return move(-1, 1)
-
-		case blockSwappingFromRight:
-			return move(1, -1)
-		}
-
-		return 0
-	}
-
-	blockRelativeY := func(b *block, fudge float32) float32 {
-		if b.state == blockDroppingFromAbove {
-			return linear(b.step+fudge, 1, -1, numDropSteps)
-		}
-		return 0
-	}
-
-	blockMatrix := func(b *block, x, y int, fudge float32) matrix4 {
-		ty := globalTranslationY + cellTranslationY*(-float32(y)+blockRelativeY(b, fudge))
-
-		ry := startRotationY + cellRotationY*(-float32(x)-blockRelativeX(b, fudge)+selectorRelativeX(fudge))
-		yq := newAxisAngleQuaternion(yAxis, toRadians(ry))
-		qm := newQuaternionMatrix(yq.normalize())
-
-		m := newTranslationMatrix(0, ty, globalTranslationZ)
-		m = m.mult(qm)
-		return m
-	}
-
-	renderSelector := func(fudge float32) {
-		sc := pulse(s.pulse+fudge, 1.0, 0.025, 0.1)
-		ty := globalTranslationY - cellTranslationY*selectorRelativeY(fudge)
-
-		m := newScaleMatrix(sc, sc, sc)
-		m = m.mult(newTranslationMatrix(0, ty, globalTranslationZ))
-		gl.UniformMatrix4fv(matrixUniform, 1, false, &m[0])
-
-		selectorMesh.drawElements()
-	}
-
-	renderCell := func(c *cell, x, y int, fudge float32) {
-		sx := float32(1)
-		bv := float32(0)
-
-		switch c.block.state {
-		case blockDroppingFromAbove:
-			sx = linear(c.block.step+fudge, 1, -0.5, numDropSteps)
-		case blockFlashing:
-			bv = pulse(c.block.step+fudge, 0, 0.5, 1.5)
-		}
-		gl.Uniform1f(brightnessUniform, bv)
-
-		m := newScaleMatrix(sx, 1, 1)
-		m = m.mult(blockMatrix(c.block, x, y, fudge))
-		gl.UniformMatrix4fv(matrixUniform, 1, false, &m[0])
-		blockMeshes[c.block.color].drawElements()
-	}
-
-	renderCellFragments := func(c *cell, x, y int, fudge float32) {
-		render := func(sc, rx, ry, rz float32, dir int) {
-			m := newScaleMatrix(sc, sc, sc)
-			m = m.mult(newTranslationMatrix(rx, ry, rz))
-			m = m.mult(blockMatrix(c.block, x, y, fudge))
-			gl.UniformMatrix4fv(matrixUniform, 1, false, &m[0])
-			fragmentMeshes[c.block.color][dir].drawElements()
-		}
-
-		ease := func(start, change float32) float32 {
-			return easeOutCubic(c.block.step+fudge, start, change, numExplodeSteps)
-		}
-
-		var bv float32
-		var av float32
-		switch c.block.state {
-		case blockCracking, blockCracked:
-			av = 1
-		case blockExploding:
-			bv = ease(0, 1)
-			av = ease(1, -1)
-		}
-		gl.Uniform1f(brightnessUniform, bv)
-		gl.Uniform1f(alphaUniform, av)
-
-		const (
-			maxCrack  = 0.03
-			maxExpand = 0.02
-			maxJitter = 0.1
-		)
-		var rs float32
-		var rt float32
-		var j float32
-		switch c.block.state {
-		case blockCracking:
-			rs = ease(1, 1+maxExpand)
-			rt = ease(0, maxCrack)
-			j = pulse(c.block.step+fudge, 0, 0.5, 1.5)
-		case blockCracked:
-			rs = 1
-			rt = maxCrack
-		case blockExploding:
-			rs = ease(1, -1)
-			rt = ease(maxCrack, math.Pi*0.75)
-		}
-
-		const szt = 0.5 // starting z translation since model is 0.5 in depth
-		wx, ex := -rt, rt
-		fz, bz := rt+szt, -rt-szt
-
-		const amp = 1
-		ny := rt + amp*float32(math.Sin(float64(rt)))
-		sy := -rt + amp*(float32(math.Cos(float64(-rt)))-1)
-
-		render(rs, wx+j, ny+j, fz, nw) // front north west
-		render(rs, ex+j, ny+j, fz, ne) // front north east
-
-		render(rs, wx+j, ny+j, bz, nw) // back north west
-		render(rs, ex+j, ny+j, bz, ne) // back north east
-
-		render(rs, wx+j, sy+j, fz, sw) // front south west
-		render(rs, ex+j, sy+j, fz, se) // front south east
-
-		render(rs, wx+j, sy+j, bz, sw) // back south west
-		render(rs, ex+j, sy+j, bz, se) // back south east
-	}
-
 	r := renderer{}
 	logFatalIfErr("renderer.init", r.init())
 
@@ -414,73 +248,9 @@ func main() {
 		}
 		fudge := float32(lag / secPerUpdate)
 
-		globalTranslationY = cellTranslationY * (4 + boardRelativeY(fudge))
-
 		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
-		r.render()
-
-		gl.Uniform1i(textureUniform, 0)
-
-		for i := 0; i <= 2; i++ {
-			gl.Uniform1f(grayscaleUniform, 0)
-			gl.Uniform1f(brightnessUniform, 0)
-			gl.Uniform1f(alphaUniform, 1)
-
-			switch i {
-			case 0:
-				gl.Disable(gl.BLEND)
-				renderSelector(fudge)
-
-			case 1:
-				gl.Enable(gl.BLEND)
-			}
-
-			for y, r := range b.rings {
-				for x, c := range r.cells {
-					switch i {
-					case 0: // draw opaque objects
-						switch c.block.state {
-						case blockStatic,
-							blockSwappingFromLeft,
-							blockSwappingFromRight,
-							blockDroppingFromAbove,
-							blockFlashing:
-							renderCell(c, x, y, fudge)
-
-						case blockCracking, blockCracked:
-							renderCellFragments(c, x, y, fudge)
-						}
-
-					case 1: // draw transparent objects
-						switch c.block.state {
-						case blockExploding:
-							renderCellFragments(c, x, y, fudge)
-						}
-					}
-				}
-			}
-
-			for y, r := range b.spareRings {
-				switch {
-				case i == 0 && y == 0: // draw opaque objects
-					gl.Uniform1f(grayscaleUniform, easeInExpo(b.riseStep+fudge, 1, -1, numRiseSteps))
-					gl.Uniform1f(brightnessUniform, 0)
-					gl.Uniform1f(alphaUniform, 1)
-					for x, c := range r.cells {
-						renderCell(c, x, y+b.ringCount, fudge)
-					}
-
-				case i == 1 && y == 1: // draw transparent objects
-					gl.Uniform1f(grayscaleUniform, 1)
-					gl.Uniform1f(brightnessUniform, 0)
-					gl.Uniform1f(alphaUniform, easeInExpo(b.riseStep+fudge, 0, 1, numRiseSteps))
-					for x, c := range r.cells {
-						renderCell(c, x, y+b.ringCount, fudge)
-					}
-				}
-			}
-		}
+		r.render(b, fudge)
 
 		win.SwapBuffers()
 		glfw.PollEvents()
--- renderer.go
+++ renderer.go
@@ -3,6 +3,7 @@ package main
 import (
 	"image"
 	"image/draw"
+	"math"
 
 	"github.com/go-gl/gl/v3.3-core/gl"
 	"github.com/golang/freetype"
@@ -74,8 +75,9 @@ func createTextImage(f *truetype.Font, text string) (*image.RGBA, error) {
 	return rgba, nil
 }
 
-func (r *renderer) render() {
+func (r *renderer) render(b *board, fudge float32) {
 	r.renderMenu()
+	r.renderBoard(b, fudge)
 }
 
 func (r *renderer) renderMenu() {
@@ -90,4 +92,244 @@ func (r *renderer) renderMenu() {
 	gl.UniformMatrix4fv(matrixUniform, 1, false, &m[0])
 	gl.Uniform1i(textureUniform, int32(r.newGameTextTexture)-1)
 	menuMesh.drawElements()
+}
+
+func (r *renderer) renderBoard(b *board, fudge float32) {
+	s := b.selector
+
+	cellRotationY := float32(360.0 / b.cellCount)
+	startRotationY := cellRotationY / 2
+	cellTranslationY := float32(2.0)
+
+	globalTranslationY := float32(0)
+	globalTranslationZ := float32(4)
+
+	selectorRelativeX := func(fudge float32) float32 {
+		move := func(delta float32) float32 {
+			return linear(s.step+fudge, float32(s.x), delta, numMoveSteps)
+		}
+
+		switch s.state {
+		case selectorMovingLeft:
+			return move(-1)
+
+		case selectorMovingRight:
+			return move(1)
+		}
+
+		return float32(s.x)
+	}
+
+	selectorRelativeY := func(fudge float32) float32 {
+		move := func(delta float32) float32 {
+			return linear(s.step+fudge, float32(s.y), delta, numMoveSteps)
+		}
+
+		switch s.state {
+		case selectorMovingUp:
+			return move(-1)
+		case selectorMovingDown:
+			return move(1)
+		}
+		return float32(s.y)
+	}
+
+	boardRelativeY := func(fudge float32) float32 {
+		return linear(b.riseStep+fudge, float32(b.y), 1, numRiseSteps)
+	}
+
+	blockRelativeX := func(b *block, fudge float32) float32 {
+		move := func(start, delta float32) float32 {
+			return linear(b.step+fudge, start, delta, numSwapSteps)
+		}
+
+		switch b.state {
+		case blockSwappingFromLeft:
+			return move(-1, 1)
+
+		case blockSwappingFromRight:
+			return move(1, -1)
+		}
+
+		return 0
+	}
+
+	blockRelativeY := func(b *block, fudge float32) float32 {
+		if b.state == blockDroppingFromAbove {
+			return linear(b.step+fudge, 1, -1, numDropSteps)
+		}
+		return 0
+	}
+
+	blockMatrix := func(b *block, x, y int, fudge float32) matrix4 {
+		ty := globalTranslationY + cellTranslationY*(-float32(y)+blockRelativeY(b, fudge))
+
+		ry := startRotationY + cellRotationY*(-float32(x)-blockRelativeX(b, fudge)+selectorRelativeX(fudge))
+		yq := newAxisAngleQuaternion(yAxis, toRadians(ry))
+		qm := newQuaternionMatrix(yq.normalize())
+
+		m := newTranslationMatrix(0, ty, globalTranslationZ)
+		m = m.mult(qm)
+		return m
+	}
+
+	renderSelector := func(fudge float32) {
+		sc := pulse(s.pulse+fudge, 1.0, 0.025, 0.1)
+		ty := globalTranslationY - cellTranslationY*selectorRelativeY(fudge)
+
+		m := newScaleMatrix(sc, sc, sc)
+		m = m.mult(newTranslationMatrix(0, ty, globalTranslationZ))
+		gl.UniformMatrix4fv(matrixUniform, 1, false, &m[0])
+
+		selectorMesh.drawElements()
+	}
+
+	renderCell := func(c *cell, x, y int, fudge float32) {
+		sx := float32(1)
+		bv := float32(0)
+
+		switch c.block.state {
+		case blockDroppingFromAbove:
+			sx = linear(c.block.step+fudge, 1, -0.5, numDropSteps)
+		case blockFlashing:
+			bv = pulse(c.block.step+fudge, 0, 0.5, 1.5)
+		}
+		gl.Uniform1f(brightnessUniform, bv)
+
+		m := newScaleMatrix(sx, 1, 1)
+		m = m.mult(blockMatrix(c.block, x, y, fudge))
+		gl.UniformMatrix4fv(matrixUniform, 1, false, &m[0])
+		blockMeshes[c.block.color].drawElements()
+	}
+
+	renderCellFragments := func(c *cell, x, y int, fudge float32) {
+		render := func(sc, rx, ry, rz float32, dir int) {
+			m := newScaleMatrix(sc, sc, sc)
+			m = m.mult(newTranslationMatrix(rx, ry, rz))
+			m = m.mult(blockMatrix(c.block, x, y, fudge))
+			gl.UniformMatrix4fv(matrixUniform, 1, false, &m[0])
+			fragmentMeshes[c.block.color][dir].drawElements()
+		}
+
+		ease := func(start, change float32) float32 {
+			return easeOutCubic(c.block.step+fudge, start, change, numExplodeSteps)
+		}
+
+		var bv float32
+		var av float32
+		switch c.block.state {
+		case blockCracking, blockCracked:
+			av = 1
+		case blockExploding:
+			bv = ease(0, 1)
+			av = ease(1, -1)
+		}
+		gl.Uniform1f(brightnessUniform, bv)
+		gl.Uniform1f(alphaUniform, av)
+
+		const (
+			maxCrack  = 0.03
+			maxExpand = 0.02
+			maxJitter = 0.1
+		)
+		var rs float32
+		var rt float32
+		var j float32
+		switch c.block.state {
+		case blockCracking:
+			rs = ease(1, 1+maxExpand)
+			rt = ease(0, maxCrack)
+			j = pulse(c.block.step+fudge, 0, 0.5, 1.5)
+		case blockCracked:
+			rs = 1
+			rt = maxCrack
+		case blockExploding:
+			rs = ease(1, -1)
+			rt = ease(maxCrack, math.Pi*0.75)
+		}
+
+		const szt = 0.5 // starting z translation since model is 0.5 in depth
+		wx, ex := -rt, rt
+		fz, bz := rt+szt, -rt-szt
+
+		const amp = 1
+		ny := rt + amp*float32(math.Sin(float64(rt)))
+		sy := -rt + amp*(float32(math.Cos(float64(-rt)))-1)
+
+		render(rs, wx+j, ny+j, fz, nw) // front north west
+		render(rs, ex+j, ny+j, fz, ne) // front north east
+
+		render(rs, wx+j, ny+j, bz, nw) // back north west
+		render(rs, ex+j, ny+j, bz, ne) // back north east
+
+		render(rs, wx+j, sy+j, fz, sw) // front south west
+		render(rs, ex+j, sy+j, fz, se) // front south east
+
+		render(rs, wx+j, sy+j, bz, sw) // back south west
+		render(rs, ex+j, sy+j, bz, se) // back south east
+	}
+
+	globalTranslationY = cellTranslationY * (4 + boardRelativeY(fudge))
+
+	gl.Uniform1i(textureUniform, 0)
+
+	for i := 0; i <= 2; i++ {
+		gl.Uniform1f(grayscaleUniform, 0)
+		gl.Uniform1f(brightnessUniform, 0)
+		gl.Uniform1f(alphaUniform, 1)
+
+		switch i {
+		case 0:
+			gl.Disable(gl.BLEND)
+			renderSelector(fudge)
+
+		case 1:
+			gl.Enable(gl.BLEND)
+		}
+
+		for y, r := range b.rings {
+			for x, c := range r.cells {
+				switch i {
+				case 0: // draw opaque objects
+					switch c.block.state {
+					case blockStatic,
+						blockSwappingFromLeft,
+						blockSwappingFromRight,
+						blockDroppingFromAbove,
+						blockFlashing:
+						renderCell(c, x, y, fudge)
+
+					case blockCracking, blockCracked:
+						renderCellFragments(c, x, y, fudge)
+					}
+
+				case 1: // draw transparent objects
+					switch c.block.state {
+					case blockExploding:
+						renderCellFragments(c, x, y, fudge)
+					}
+				}
+			}
+		}
+
+		for y, r := range b.spareRings {
+			switch {
+			case i == 0 && y == 0: // draw opaque objects
+				gl.Uniform1f(grayscaleUniform, easeInExpo(b.riseStep+fudge, 1, -1, numRiseSteps))
+				gl.Uniform1f(brightnessUniform, 0)
+				gl.Uniform1f(alphaUniform, 1)
+				for x, c := range r.cells {
+					renderCell(c, x, y+b.ringCount, fudge)
+				}
+
+			case i == 1 && y == 1: // draw transparent objects
+				gl.Uniform1f(grayscaleUniform, 1)
+				gl.Uniform1f(brightnessUniform, 0)
+				gl.Uniform1f(alphaUniform, easeInExpo(b.riseStep+fudge, 0, 1, numRiseSteps))
+				for x, c := range r.cells {
+					renderCell(c, x, y+b.ringCount, fudge)
+				}
+			}
+		}
+	}
 }
