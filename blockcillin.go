1949071e824cb26fc55066d14a97d00981eeeda5
Brian Muramatsu <btmura@gmail.com>
2015-12-20 11:02:00 -0800

    integrate portaudio sine wave example

--- blockcillin.go
+++ blockcillin.go
@@ -4,7 +4,9 @@ package main
 
 import (
 	"log"
+	"math"
 	"runtime"
+	"time"
 
 	"github.com/go-gl/gl/v3.3-core/gl"
 	"github.com/go-gl/glfw/v3.1/glfw"
@@ -12,6 +14,8 @@ import (
 )
 
 const secPerUpdate = 1.0 / 60.0
+
+const sampleRate = 44100
 
 func init() {
 	// This is needed to arrange that main() runs on the main thread.
@@ -36,8 +40,17 @@ func main() {
 	log.Printf("OpenGL version: %s", gl.GoStr(gl.GetString(gl.VERSION)))
 
 	logFatalIfErr("portaudio.Initialize", portaudio.Initialize())
-	defer logFatalIfErr("portaudio.Terminate", portaudio.Terminate())
+	defer func() {
+		logFatalIfErr("portaudio.Terminate", portaudio.Terminate())
+	}()
 	log.Printf("PortAudio version: %d %s", portaudio.Version(), portaudio.VersionText())
+
+	// PortAudio sample from https://github.com/gordonklaus/portaudio/blob/master/examples/stereoSine.go
+	s := newStereoSine(256, 320, sampleRate)
+	defer s.Close()
+	logFatalIfErr("s.Start", s.Start())
+	time.Sleep(2 * time.Second)
+	logFatalIfErr("s.Stop", s.Stop())
 
 	rr := newRenderer()
 
@@ -77,5 +90,30 @@ func main() {
 func logFatalIfErr(tag string, err error) {
 	if err != nil {
 		log.Fatalf("%s: %v", tag, err)
+	}
+}
+
+// PortAudio sample from https://github.com/gordonklaus/portaudio/blob/master/examples/stereoSine.go
+
+type stereoSine struct {
+	*portaudio.Stream
+	stepL, phaseL float64
+	stepR, phaseR float64
+}
+
+func newStereoSine(freqL, freqR, sampleRate float64) *stereoSine {
+	s := &stereoSine{nil, freqL / sampleRate, 0, freqR / sampleRate, 0}
+	var err error
+	s.Stream, err = portaudio.OpenDefaultStream(0, 2, sampleRate, 0, s.processAudio)
+	logFatalIfErr("portaudio.OpenDefaultStream", err)
+	return s
+}
+
+func (g *stereoSine) processAudio(out [][]float32) {
+	for i := range out[0] {
+		out[0][i] = float32(math.Sin(2 * math.Pi * g.phaseL))
+		_, g.phaseL = math.Modf(g.phaseL + g.stepL)
+		out[1][i] = float32(math.Sin(2 * math.Pi * g.phaseR))
+		_, g.phaseR = math.Modf(g.phaseR + g.stepR)
 	}
 }
