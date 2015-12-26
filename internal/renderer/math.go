92ab088b3b5100e8ed5c079026e59b04b24fd081
Brian Muramatsu <btmura@gmail.com>
2015-12-25 23:46:25 -0800

    remove unused easing functions

--- internal/renderer/math.go
+++ internal/renderer/math.go
@@ -2,32 +2,12 @@ package renderer
 
 import "math"
 
-func linear(time, start, change, duration float32) float32 {
-	t := time / duration
-	return change*t + start
-}
-
 func linear2(t, start, change float32) float32 {
 	return change*t + start
 }
 
-func easeInCubic(time, start, change, duration float32) float32 {
-	t := time / duration
-	return change*t*t*t + start
-}
-
-func easeInExpo(time, start, change, duration float32) float32 {
-	t := time / duration
-	return change*float32(math.Pow(2, float64(10*(t-1)))) + start
-}
-
 func easeInExpo2(t, start, change float32) float32 {
 	return change*float32(math.Pow(2, float64(10*(t-1)))) + start
-}
-
-func easeOutCubic(time, start, change, duration float32) float32 {
-	t := time/duration - 1
-	return change*(t*t*t+1) + start
 }
 
 func easeOutCubic2(t, start, change float32) float32 {
