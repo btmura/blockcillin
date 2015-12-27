e377c09eb8a8882d4f795934e172f66f657715e9
Brian Muramatsu <btmura@gmail.com>
2015-12-27 12:40:30 -0800

    format time properly in hud

--- internal/renderer/hud.go
+++ internal/renderer/hud.go
@@ -1,6 +1,7 @@
 package renderer
 
 import (
+	"fmt"
 	"strconv"
 
 	"github.com/btmura/blockcillin/internal/game"
@@ -27,7 +28,7 @@ func renderHUD(g *game.Game, fudge float32) {
 		}
 
 		x = float32(winWidth)/4*float32(i) - valWidth/2
-		y -= valHeight
+		y -= valHeight * 1.5
 		for _, rune := range val {
 			text := hudRuneText[rune]
 			text.render(x, y)
@@ -37,7 +38,25 @@ func renderHUD(g *game.Game, fudge float32) {
 		i++
 	}
 
-	renderText(game.HUDItemSpeed, strconv.Itoa(g.HUD.Speed))
-	renderText(game.HUDItemTime, strconv.Itoa(g.HUD.TimeSec))
-	renderText(game.HUDItemScore, strconv.Itoa(g.HUD.Score))
+	renderText(game.HUDItemSpeed, formattedSpeed(g))
+	renderText(game.HUDItemTime, formattedTime(g))
+	renderText(game.HUDItemScore, formattedScore(g))
+}
+
+func formattedSpeed(g *game.Game) string {
+	return strconv.Itoa(g.HUD.Speed)
+}
+
+func formattedTime(g *game.Game) string {
+	h := g.HUD.TimeSec / 3600
+	m := g.HUD.TimeSec / 60
+	s := g.HUD.TimeSec % 60
+	if h != 0 {
+		return fmt.Sprintf("%0.2d%0.2d:%0.2d", h, m, s)
+	}
+	return fmt.Sprintf("%0.2d:%0.2d", m, s)
+}
+
+func formattedScore(g *game.Game) string {
+	return strconv.Itoa(g.HUD.Score)
 }
