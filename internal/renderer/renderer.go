1e620320aa741ba94b60a7908c39820d4c93fef9
Brian Muramatsu <btmura@gmail.com>
2015-12-26 19:56:20 -0800

    move renderer text code to separate file

--- internal/renderer/renderer.go
+++ internal/renderer/renderer.go
@@ -15,8 +15,6 @@ import (
 	"github.com/btmura/blockcillin/internal/game"
 	"github.com/go-gl/gl/v3.3-core/gl"
 	"github.com/golang/freetype"
-	"github.com/golang/freetype/truetype"
-	"golang.org/x/image/font"
 )
 
 var (
@@ -86,12 +84,6 @@ var (
 	menuItemText  map[game.MenuItem]renderableText
 	speedText     renderableText
 )
-
-type renderableText struct {
-	texture uint32
-	width   float32
-	height  float32
-}
 
 func Init() error {
 	if err := gl.Init(); err != nil {
@@ -252,7 +244,7 @@ func Init() error {
 		textureUnit++
 	}
 
-	speedText, err = createText(textureUnit, font, "SPEED", hudFontSize, hudTextColor)
+	speedText, err = createText(textureUnit, font, "S P E E D", hudFontSize, hudTextColor)
 
 	gl.Enable(gl.CULL_FACE)
 	gl.CullFace(gl.BACK)
@@ -281,56 +273,6 @@ func createAssetTexture(textureUnit uint32, name string) (uint32, error) {
 	rgba := image.NewRGBA(img.Bounds())
 	draw.Draw(rgba, rgba.Bounds(), img, image.Point{0, 0}, draw.Src)
 	return createTexture(textureUnit, rgba)
-}
-
-func createText(textureUnit uint32, f *truetype.Font, text string, fontSize int, color color.Color) (renderableText, error) {
-	rgba, w, h, err := createTextImage(f, text, fontSize, color)
-	if err != nil {
-		return renderableText{}, err
-	}
-
-	t, err := createTexture(textureUnit, rgba)
-	if err != nil {
-		return renderableText{}, err
-	}
-	return renderableText{t, w, h}, nil
-}
-
-func createTextImage(f *truetype.Font, text string, fontSize int, color color.Color) (*image.RGBA, float32, float32, error) {
-	// 1 pt = 1/72 in, 72 dpi = 1 in
-	const dpi = 72
-
-	fg, bg := image.NewUniform(color), image.Transparent
-
-	c := freetype.NewContext()
-	c.SetFont(f)
-	c.SetDPI(dpi)
-	c.SetFontSize(float64(fontSize))
-	c.SetSrc(fg)
-	c.SetHinting(font.HintingFull)
-
-	// 1. Draw within small bounds to figure out bounds.
-	// 2. Draw within final bounds.
-
-	var rgba *image.RGBA
-	w, h := 10, fontSize
-	for i := 0; i < 2; i++ {
-		rgba = image.NewRGBA(image.Rect(0, 0, w, h))
-		draw.Draw(rgba, rgba.Bounds(), bg, image.ZP, draw.Src)
-
-		c.SetClip(rgba.Bounds())
-		c.SetDst(rgba)
-
-		pt := freetype.Pt(0, int(c.PointToFixed(float64(fontSize))>>6))
-		end, err := c.DrawString(text, pt)
-		if err != nil {
-			return nil, 0, 0, err
-		}
-
-		w = int(end.X >> 6)
-	}
-
-	return rgba, float32(w), float32(h), nil
 }
 
 func Render(g *game.Game, fudge float32) {
